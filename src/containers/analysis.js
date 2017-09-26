b3e554fcc9d580f6441218938da7dcca6fe75e48
ChristopherHardman <cwhardman@gmail.com>
2017-09-26 21:28:03 +0200

    Updates with further progress on bubble chart

--- src/containers/analysis.js
+++ src/containers/analysis.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './comparison.css';
+import { connect } from 'react-redux'
 import Header from '../components/header'
 import Headline from '../components/headline'
 import WordCount from '../analysisComponents/wordCount'
 import Details from '../components/details'
 import Summary from '../analysisComponents/summary'
 import Sentiment from '../analysisComponents/sentiment'
+import Progress from '../components/progress'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
 
 class Analysis extends React.Component {
     render () {
+      // if (this.props.sentiment.score_tag === undefined) return (
+      //   <MuiThemeProvider>
+      //     <div>
+      //     <Header />
+      //     <Headline />
+      //     <Progress />
+      //     <Details />
+      //     </div>
+      //   </MuiThemeProvider>
+      // );
+      // else {
       return (
         <div>
           <Header />
@@ -22,6 +38,14 @@ class Analysis extends React.Component {
         </div>
       )
     }
+  }
+// }
+
+const mapStateToProps = (state) => {
+  return {
+    sentiment: state.sentiment,
+
+  }
 }
 
-export default Analysis;
+export default connect(mapStateToProps, null)  (Analysis);
